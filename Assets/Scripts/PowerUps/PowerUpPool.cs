bf0a01ff72ab9eda315df8680b711e1dd8f1b130
Yash Sadhukhan <yskhan61@gmail.com>
2024-04-24 21:17:07 +0530

    Create Powerup Pool
    Create powerup pool
    Write logic such that this pool can handle different types of powerups

---
+++ Assets/Scripts/PowerUps/PowerUpPool.cs
@@ -0,0 +1,34 @@
+using CosmicCuration.Utilities;
+
+namespace CosmicCuration.PowerUps
+{
+    public class PowerUpPool : GenericObjectPool<PowerUpController>
+    {
+        private PowerUpData powerUpData;
+        public PowerUpController GetPowerUp<T>(PowerUpData powerUpData) where T : PowerUpController
+        {
+            this.powerUpData = powerUpData;
+            return GetItem<T>();
+        }
+
+        protected override PowerUpController CreateItem<T>()
+        {
+            if (typeof(T) == typeof(Shield))
+            {
+                return new Shield(powerUpData);
+            }
+            else if (typeof(T) == typeof(RapidFire))
+            {
+                return new RapidFire(powerUpData);
+            }
+            else if (typeof(T) == typeof(DoubleTurret))
+            {
+                return new DoubleTurret(powerUpData);
+            }
+            else
+            {
+                throw new System.NotImplementedException("PowerUp type not implemented");
+            }
+        }
+    }
+}
